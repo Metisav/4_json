42b8eef4835b6ad0879e343151fbf5ea8d3f259b
metisav <metisav@gmail.com>
2016-12-28 23:32:35 +0500

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 4_json
+
+## PrettyPrint for JSON
+
+Using JSON library print JSON pretty printed style in console.
+
+## Example using
+
+python3 pprint_json.py
+
+{Enter filename raw json}
+
+Example output:
+
+
--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,16 @@ import json
 
 
 def load_data(filepath):
-    pass
+    data = open(filepath, 'r').read()
+    json_data = json.loads(''.join(data))
+    return json_data
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    path = input("введите имя файла с данными\n")
+    data = load_data(path)
+    print(pretty_print_json(data))
